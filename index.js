b74f13d72e9bfe2832a6445f0f37cd96f7b74e50
aymaneyamahafes-cmyk <aymane.yamahafes@gmail.com>
2026-05-16 16:40:16 +0100

    lff

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-console.log("Script loaded successfully!");
-const btn = document.getElementById("btn");
-const nom = document.getElementById("name");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+const savedusername = JSON.parse(localStorage.getItem("username") || "null");
+if (savedusername) {
+    nom.value = savedusername || "";
+    email.value = savedusername || "";
+    password.value = savedusername || "";
+}
 
-console.log(btn);
-console.log(nom.value);
-console.log(email.value);
-console.log(password.value);
-btn.addEventListener("click", function() {
-    console.log("Button clicked!");
-    console.log("Nom: " + nom.value);
-    console.log("Email: " + email.value);
-    console.log("Password: " + password.value);
-    alert("bonne inscription");
-    
-    if (nom.value === "") {
-        alert("Nom est manquant");
-        return;
-    }
+button.addEventListener("click", function(event){
+    event.preventDefault();
 
-    if (email.value === "") {
-        alert("Email est manquant");
-        return;
-    }
-
-    if (!email.value.includes("@")) {
-        alert("Email doit contenir @");
-        return;
-    }
-
-    if (password.value === "") {
-        alert("Mot de passe est manquant");
-        return;
-    }
-
-    if (password.value.length < 6) {
-        alert("Mot de passe doit contenir au moins 6 caractères");
-        return;
-    }
-    alert(nom.value + " " + email.value + " " + password.value);
+    const user ={
+        username: nom.value,
+        email: email.value,
+        password: password.value
+    };
+    localStorage.setItem("username", JSON.stringify(user));
+    alert("Name: " + nom.value + "\nEmail: " + email.value + "\nPassword: " + password.value);
 });
